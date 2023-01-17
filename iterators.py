f5408e62d875fca954c3f917f99cb288adf816cf
Dmitriy Matveev <matveevd34@gmail.com>
2023-01-17 12:13:33 +0300

    Initial commit

---
+++ iterators.py
@@ -0,0 +1,41 @@
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+
+    def __iter__(self):
+        self.list_iter = iter(self.list_of_list)
+        self.nested_list = []
+        self.counter = -1
+        return self
+
+    def __next__(self):
+        self.counter += 1
+        if len(self.nested_list) == self.counter:
+            self.nested_list = None
+            self.counter = 0
+            while not self.nested_list:
+                self.nested_list = next(self.list_iter)
+        return self.nested_list[self.counter]
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
